bc09d06d65143b6ba3b64d6703448c817f08e4ca
Pieter Pas <pieter.pas@outlook.com>
2022-06-28 19:08:29 +0200

    Nox add editable install test

--- noxfile.py
+++ noxfile.py
@@ -2,6 +2,7 @@ import shutil
 import nox
 import os
 
+
 @nox.session
 def example_projects(session: nox.Session):
     session.install("-U", "pip", "build", "pytest")
@@ -12,13 +13,11 @@ def example_projects(session: nox.Session):
     session.env["PIP_FIND_LINKS"] = os.path.abspath(dist_dir)
     session.install("py-build-cmake~=0.0.11a0")
     with session.chdir("examples/minimal"):
-        session.install("mypy")
         shutil.rmtree('.py-build-cmake_cache', ignore_errors=True)
         session.run("python", "-m", "build", ".")
         session.install(".")
         session.run("pytest")
     with session.chdir("examples/pybind11-project"):
-        session.install("pybind11", "pybind11_stubgen", "mypy")
         shutil.rmtree('.py-build-cmake_cache', ignore_errors=True)
         session.run("python", "-m", "build", ".")
         session.install(".")
@@ -27,6 +26,22 @@ def example_projects(session: nox.Session):
         shutil.rmtree('.py-build-cmake_cache', ignore_errors=True)
         session.run("python", "-m", "build", ".")
         session.install(".")
+        session.run("pytest")
+
+
+@nox.session
+def editable(session: nox.Session):
+    session.install("-U", "pip", "build", "pytest")
+    dist_dir = os.getenv('PY_BUILD_CMAKE_WHEEL_DIR')
+    if dist_dir is None:
+        session.run("python", "-m", "build", ".")
+        dist_dir = "dist"
+    session.env["PIP_FIND_LINKS"] = os.path.abspath(dist_dir)
+    session.install("py-build-cmake~=0.0.11a0")
+    with session.chdir("examples/pybind11-project"):
+        shutil.rmtree('.py-build-cmake_cache', ignore_errors=True)
+        session.run("python", "-m", "build", ".")
+        session.install("-e", ".")
         session.run("pytest")
 
 
