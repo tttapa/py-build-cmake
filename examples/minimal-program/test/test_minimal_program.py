fc39971bdc1872ec78c3d2d664e1af9dc3981187
Pieter Pas <pieter.pas@outlook.com>
2022-07-31 17:07:06 +0200

    Python 3.6 support

--- examples/minimal-program/test/test_minimal_program.py
+++ examples/minimal-program/test/test_minimal_program.py
@@ -2,5 +2,5 @@ import subprocess
 import os
 
 def test_run_program():
-    res = subprocess.run(["minimal_program"], capture_output=True)
+    res = subprocess.run(["minimal_program"], stdout=subprocess.PIPE)
     assert res.stdout.decode("utf-8") == "Hello, world!" + os.linesep
--- examples/pybind11-project/cmake/Pybind11Stubgen.cmake
+++ examples/pybind11-project/cmake/Pybind11Stubgen.cmake
@@ -1,8 +1,15 @@
 function(pybind11_stubgen target)
 
     find_package(Python3 REQUIRED COMPONENTS Interpreter)
+    execute_process(COMMAND ${Python3_EXECUTABLE}
+        -c "import os; print(os.pathsep)"
+        OUTPUT_VARIABLE PY_PATH_SEP
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+    string(REPLACE ";" "\\;" PY_PATH_SEP "${PY_PATH_SEP}")
+    set(PY_PATH "$<TARGET_FILE_DIR:${target}>${PY_PATH_SEP}$ENV{PYTHONPATH}")
     add_custom_command(TARGET ${target} POST_BUILD
-        COMMAND ${Python3_EXECUTABLE} -m pybind11_stubgen
+        COMMAND ${CMAKE_COMMAND} -E env PYTHONPATH="${PY_PATH}"
+            ${Python3_EXECUTABLE} -m pybind11_stubgen
                 $<TARGET_FILE_BASE_NAME:${target}>
                 --bare-numpy-ndarray
                 --no-setup-py
