15cc0ba9ab7e01ea361a81e71affaf94694bc9a9
Pieter Pas <pieter.pas@outlook.com>
2023-07-24 17:39:40 +0200

    Flush stdout in verbose mode before calling run()

--- src/py_build_cmake/cmd_runner.py
+++ src/py_build_cmake/cmd_runner.py
@@ -18,6 +18,7 @@ class CommandRunner:
         if self.verbose:
             pprint([*args])
             pprint(kwargs)
+            print(flush=True)
         elif self.dry:
             from shlex import join
             print(join(args[0]))
