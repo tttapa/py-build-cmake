3b48909aa714e4070f636165f06c5643ea2e2640
Pieter Pas <pieter.pas@outlook.com>
2023-10-14 23:17:01 +0200

    [Tests] no normalization of Posix paths

--- tests/test_config_load.py
+++ tests/test_config_load.py
@@ -6,7 +6,7 @@ from py_build_cmake.config.load import Config, ConfigError, process_config
 
 
 def test_process_config_no_cmake():
-    pyproj_path = PurePosixPath(os.path.normpath("/project/pyproject.toml"))
+    pyproj_path = PurePosixPath("/project/pyproject.toml")
     pyproj = {
         "project": {"name": "foobar", "version": "0.0.1"},
         "tool": {"some-other-tool": {}, "py-build-cmake": {}},
@@ -23,7 +23,7 @@ def test_process_config_no_cmake():
 
 
 def test_process_config_no_cmake_namespace_wrong_editable_mode():
-    pyproj_path = PurePosixPath(os.path.normpath("/project/pyproject.toml"))
+    pyproj_path = PurePosixPath("/project/pyproject.toml")
     pyproj = {
         "project": {"name": "foobar", "version": "0.0.1"},
         "tool": {
