1f11a88fabc79fa02e9983de46b712f532d3406a
Pieter Pas <pieter.pas@outlook.com>
2024-01-27 22:24:53 +0100

    Version 0.2.0a10

--- noxfile.py
+++ noxfile.py
@@ -32,7 +32,7 @@ if sys.version_info < (3, 8):
 else:
     import sysconfig as dist_sysconfig
 
-version = "0.2.0a10.dev0"
+version = "0.2.0a10"
 project_dir = Path(__file__).resolve().parent
 
 examples = "minimal-program", "pybind11-project", "nanobind-project", "minimal"
