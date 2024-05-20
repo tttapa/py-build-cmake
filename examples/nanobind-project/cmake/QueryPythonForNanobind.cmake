ec48e8eabdadf64ff5a24bfa7c1ae6b5ba0188a5
Pieter Pas <pieter.p.dev@outlook.com>
2024-05-20 15:20:43 +0200

    Update QueryPythonForNanobind.cmake

--- examples/nanobind-project/cmake/QueryPythonForNanobind.cmake
+++ examples/nanobind-project/cmake/QueryPythonForNanobind.cmake
@@ -1,18 +1,24 @@
 option(USE_GLOBAL_NANOBIND "Don't query Python to find nanobind" Off)
 mark_as_advanced(USE_GLOBAL_NANOBIND)
 
+# Returns the parent folder of the current file.
+function(find_nanobind_python_first_dir var)
+    set(${var} ${CMAKE_CURRENT_FUNCTION_LIST_DIR} PARENT_SCOPE)
+endfunction()
+
 # First tries to find Python 3.8, then tries to import the nanobind module to
 # query the CMake config location, and finally imports nanobind using
-# find_package(nanobind REQUIRED CONFIG CMAKE_FIND_ROOT_PATH_BOTH).
+# find_package(nanobind REQUIRED CONFIG CMAKE_FIND_ROOT_PATH_BOTH). It also
+# allows setting (and fixing) NB_SUFFIX and NB_SUFFIX_S when cross-compiling.
+#
 # This is a macro rather than a function to make the variables set by
 # find_package(Python) available to the nanobind functions when called in the
 # main project (specifically, the nanobind_build_library function explicitly
-# adds the ${Python_INCLUDE_DIRS} to the nanobind include directories). It also
-# allows un-setting NB_SUFFIX when cross-compiling.
+# adds the ${Python_INCLUDE_DIRS} to the nanobind include directories).
 macro(find_nanobind_python_first)
 
     # Find Python
-    if (CMAKE_CROSSCOMPILING AND FALSE) # https://gitlab.kitware.com/cmake/cmake/-/issues/25145#note_1396533
+    if (CMAKE_CROSSCOMPILING AND NOT (APPLE AND "$ENV{CIBUILDWHEEL}" STREQUAL "1"))
         find_package(Python 3.8 REQUIRED
             COMPONENTS Development.Module
             OPTIONAL_COMPONENTS Development.SABIModule)
@@ -23,57 +29,17 @@ macro(find_nanobind_python_first)
     endif()
 
     # Tweak extension suffix and debug ABI when cross-compiling
-    if (CMAKE_CROSSCOMPILING)
-        if (NOT PY_BUILD_EXT_SUFFIX AND DEFINED TOOLCHAIN_Python_EXT_SUFFIX)
-            set(PY_BUILD_EXT_SUFFIX ${TOOLCHAIN_Python_EXT_SUFFIX})
-        endif()
-        # SETUPTOOLS_EXT_SUFFIX environment variable
-        if (NOT PY_BUILD_EXT_SUFFIX AND DEFINED ENV{SETUPTOOLS_EXT_SUFFIX})
-            message(STATUS "Setting PY_BUILD_EXT_SUFFIX to "
-                "ENV{SETUPTOOLS_EXT_SUFFIX}: $ENV{SETUPTOOLS_EXT_SUFFIX}")
-            set(PY_BUILD_EXT_SUFFIX $ENV{SETUPTOOLS_EXT_SUFFIX})
-        endif()
-        # If that still didn't work, use the Python_SOABI variable:
-        if (NOT PY_BUILD_EXT_SUFFIX AND Python_SOABI)
-            message(STATUS "Determining Python extension suffix based on "
-                    "Python_SOABI.")
-            if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
-                set(PY_BUILD_EXTENSION ".pyd")
-            else()
-                set(PY_BUILD_EXTENSION "${CMAKE_SHARED_MODULE_SUFFIX}")
-            endif()
-            set(PY_BUILD_EXT_SUFFIX ".${Python_SOABI}${PY_BUILD_EXTENSION}")
-        endif()
-        # Sanity checks:
-        if (NOT PY_BUILD_EXT_SUFFIX)
-            message(FATAL_ERROR "Unable to determine extension suffix.\
-                Try manually setting PY_BUILD_EXT_SUFFIX.")
-        endif()
-        if (Python_SOABI AND
-                NOT PY_BUILD_EXT_SUFFIX MATCHES "\\.${Python_SOABI}\\.")
-            message(WARNING "PY_BUILD_EXT_SUFFIX (${PY_BUILD_EXT_SUFFIX}) "
-                "does not match Python_SOABI (${Python_SOABI})")
-        endif()
-        # Check the debug ABI:
-        if (NOT PY_BUILD_DEBUG_ABI AND DEFINED TOOLCHAIN_Python_DEBUG_ABI)
-            set(PY_BUILD_DEBUG_ABI ${TOOLCHAIN_Python_DEBUG_ABI})
-        endif()
-        # Otherwise, try to deduce it from the SOABI:
-        if (NOT DEFINED PY_BUILD_DEBUG_ABI)
-            if (PY_BUILD_EXT_SUFFIX MATCHES "[0-9]+d-")
-                set(PY_BUILD_DEBUG_ABI true)
-            else()
-                set(PY_BUILD_DEBUG_ABI false)
-            endif()
-        endif()
-        # Cache the result:
-        set(PY_BUILD_EXT_SUFFIX ${PY_BUILD_EXT_SUFFIX} CACHE STRING
-            "The extension for Python extension modules")
-        set(PY_BUILD_DEBUG_ABI ${PY_BUILD_DEBUG_ABI} CACHE BOOL
-            "Whether to compile for a debug version of Python")
-        # Override nanobind-config.cmake's NB_SUFFIX variable:
-        message(STATUS "Python extension suffix: ${PY_BUILD_EXT_SUFFIX}")
-        set(NB_SUFFIX ${PY_BUILD_EXT_SUFFIX} CACHE INTERNAL "" FORCE)
+    if (CMAKE_CROSSCOMPILING AND NOT (DEFINED CACHE{NB_SUFFIX}
+                                      AND DEFINED CACHE{NB_SUFFIX_S}))
+        find_nanobind_python_first_dir(FNBF_CURRENT_DIR)
+        include(${FNBF_CURRENT_DIR}/nanobindGuessPythonExtSuffix.cmake)
+        nanobind_guess_python_module_extension(Python)
+    endif()
+    # Save for later ...
+    if (CMAKE_CROSSCOMPILING AND (DEFINED NB_SUFFIX
+                                  AND DEFINED NB_SUFFIX_S))
+        set(PY_BUILD_CMAKE_NB_SUFFIX "${NB_SUFFIX}" CACHE STRING "")
+        set(PY_BUILD_CMAKE_NB_SUFFIX_S "${NB_SUFFIX_S}" CACHE STRING "")
     endif()
 
     # Query Python to see if it knows where the pybind11 root is
@@ -100,10 +66,14 @@ macro(find_nanobind_python_first)
     # native version is fine
     find_package(nanobind ${ARGN} REQUIRED CONFIG CMAKE_FIND_ROOT_PATH_BOTH)
 
-    if (CMAKE_CROSSCOMPILING)
-        # nanobind-config.cmake unconditionally sets this, but we want it to
-        # use our cache variable instead.
+    # nanobind-config.cmake unconditionally sets these variables, but we want it
+    # to use our variables instead.
+    if (CMAKE_CROSSCOMPILING AND (DEFINED CACHE{PY_BUILD_CMAKE_NB_SUFFIX}
+                                  AND DEFINED CACHE{PY_BUILD_CMAKE_NB_SUFFIX_S}))
         unset(NB_SUFFIX)
+        unset(NB_SUFFIX_S)
+        set(NB_SUFFIX "$CACHE{PY_BUILD_CMAKE_NB_SUFFIX}" CACHE INTERNAL "")
+        set(NB_SUFFIX_S "$CACHE{PY_BUILD_CMAKE_NB_SUFFIX_S}" CACHE INTERNAL "")
     endif()
 
 endmacro()
