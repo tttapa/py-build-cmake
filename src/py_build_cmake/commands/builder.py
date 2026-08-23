e628e933971f884fc18feb9544d4f1ee7066fa9f
Pieter Pas <pieter.p.dev@outlook.com>
2026-08-23 14:26:53 +0200

    Always enable t flag in Python3_FIND_ABI, even if minimum CMake is too old

--- src/py_build_cmake/commands/builder.py
+++ src/py_build_cmake/commands/builder.py
@@ -222,14 +222,15 @@ class Builder(ABC):
 
     @staticmethod
     def _get_python_abi_tuple(abiflags, cmake_version):
-        has_t_flag = NormalizedVersion("3.30") <= NormalizedVersion(cmake_version)
-        dmu = "dmut" if has_t_flag else "dmu"
-        if "t" in abiflags and not has_t_flag:
+        have_t_flag = "t" in abiflags
+        supports_t_flag = NormalizedVersion("3.30") <= NormalizedVersion(cmake_version)
+        if have_t_flag and not supports_t_flag:
             msg = "CMake version %s does not support the free-threaded ABI, but "
             msg += "the current interpreter requires it. You should upgrade to "
             msg += "CMake 3.30 or later, and upgrade the CMake minimum required "
             msg += "version or policies in your CMakeLists.txt file accordingly."
             logger.warning(msg, cmake_version)
+        dmu = "dmut" if supports_t_flag or have_t_flag else "dmu"
         return tuple("ON" if c in abiflags else "OFF" for c in dmu)
 
     def _get_native_python_abi_tuple(self, cmake_version):
@@ -302,6 +303,7 @@ class Builder(ABC):
         # TODO: FIND_ABI seems to confuse CMake on older versions, so only set
         #       it if we need to enable the free-threaded ABI
         abi = self.get_native_python_abi_tuple()
+        logger.debug("Native Python find ABI tuple: %s", ";".join(abi))
         if len(abi) == 4 and abi[-1] == "ON":
             yield Option(prefix + "_FIND_ABI", ";".join(abi))
 
@@ -340,6 +342,7 @@ class Builder(ABC):
         # TODO: FIND_ABI seems to confuse CMake on older versions, so only set
         #       it if we need to enable the free-threaded ABI
         abi = self.get_cross_python_abi_tuple()
+        logger.debug("Cross Python find ABI tuple: %s", ";".join(abi))
         if len(abi) == 4 and abi[-1] == "ON":
             yield Option(prefix + "_FIND_ABI", ";".join(abi))
 
